7eb50fddc31d4054777449828033b12f530af93f
Angela Monsalve <angelamonsalve@wposs.com>
2021-12-22 11:44:19 -0500

    Se crea la clase respuesta con su constructor, getters y setters.

---
+++ juegoPreguntas/src/juegopreguntas/model/Respuesta.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegopreguntas.model;
+
+
+public class Respuesta {
+    
+    private int id;
+    private String descripcion;
+    private boolean estado;
+
+    public Respuesta() {
+    }
+
+    public Respuesta(int id, String descripcion, boolean estado) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+}
